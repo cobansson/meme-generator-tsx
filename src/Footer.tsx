847e50d494329a4c83615d779702817e180e515c
cobansson <b.engincobanoglu@gmail.com>
2023-10-23 11:21:42 +0200

    second commit

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,7 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact, faJsSquare } from "@fortawesome/free-brands-svg-icons";
+import typeScriptImg from "./images/typescript.png";
+
 export default function Footer() {
-    return (
-        <footer>
-            <h1>Powered by TS</h1>
-        </footer>
-    )
+  return (
+    <footer>
+      <h1>Powered by</h1>
+      <div className="logo-container">
+        <FontAwesomeIcon icon={faReact} className="react-logo" spin />
+        <FontAwesomeIcon icon={faJsSquare} className="js-logo" />
+        <img src={typeScriptImg} className="ts-logo"/>
+      </div>
+    </footer>
+  );
 }
